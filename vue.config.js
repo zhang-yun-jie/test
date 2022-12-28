7959fa94eb4ffbb7fd7fae5873ad87b8a8e3cb12
zhang-yun-jie <yun88xiao88chu@qq.com>
2022-12-28 16:44:31 +0800

    .

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,25 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-});
+// const { defineConfig } = require("@vue/cli-service");
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+// });
+const path = require("path");
+const isProd = process.env.NODE_ENV === "production";
+module.exports = {
+  publicPath: isProd ? "/test" : "/", // 打包后的文件引入路劲 平台
+  outputDir: "dist",
+  assetsDir: "static",
+  productionSourceMap: false, // 去除生成环境source map
+  devServer: {
+    open: false,
+    host: "0.0.0.0",
+    hot: "only",
+    static: {
+      directory: path.join(__dirname, "static"),
+    },
+    devMiddleware: {
+      publicPath: isProd ? "/test" : "/",
+    },
+    port: 8080,
+    compress: true,
+  },
+};
